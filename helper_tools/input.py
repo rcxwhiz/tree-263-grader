7ece8e7cabbd26d8fd01005319a5ec4449197225
josh-desktop <rcxwhiz@gmail.com>
2020-02-08 00:18:46 -0700

    Added new levels of args validation.

--- helper_tools/input.py
+++ helper_tools/input.py
@@ -1,14 +1,33 @@
+import os
 import sys
+
+supported_files = ['py', 'xlsx']
+hw_directory = r'C:\Users\josh-desktop\OneDrive\School\CH EN 263 TA'
 
 
 def exit_msg(msg):
+	print('[ERROR]')
 	print(msg)
 	input('Press any key to exit...')
 	sys.exit()
 
 
 def validate_args(args):
-	if len(args) != 3:
-		exit_msg('Arguments should be [py/xlsx] [prob num]')
-	if args[1] != 'py' and args[1] != 'xlsx':
-		exit_msg('Only py and xlsx file formats are supported')
+	if len(args) != 4:
+		exit_msg(f'Arguments should be [hw num] {supported_files} [prob num]')
+	if args[2] not in supported_files:
+		exit_msg(f'File formats supported: {" ".join(supported_files)}')
+	try:
+		os.chdir(os.path.join(hw_directory, 'HW ' + args[1]))
+	except FileNotFoundError:
+		exit_msg(f'Could not find the HW folder:\n'
+		         f'{os.path.join(hw_directory, "HW " + args[1])}')
+	hw_dirs = os.listdir('.')
+	for cdir in hw_dirs:
+		if 'Gradebook Bundled Download' in cdir:
+			os.chdir(cdir)
+			print(f'Finding hw files in:\n'
+			      f'{os.getcwd()}')
+			return None
+	exit_msg(f'Unable to find a Gradebook Bundled Download folder in:\n'
+	         f'{os.getcwd()}')
--- main.py
+++ main.py
@@ -2,5 +2,18 @@ import sys
 
 import helper_tools
 
+
+def py_grader(prob):
+	print(f'Grading py problem {prob}')
+
+
+def xlsx_grader(prob):
+	print(f'Grading xlsx problem {prob}')
+
+
 if __name__ == '__main__':
 	helper_tools.input.validate_args(sys.argv)
+	if sys.argv[2] == 'py':
+		py_grader(sys.argv[3])
+	if sys.argv[2] == 'xlsx':
+		xlsx_grader(sys.argv[3])
