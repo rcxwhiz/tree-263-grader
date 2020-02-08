e8ca7fcdcb6f045338154cc064187608b6d21ffe
josh-desktop <rcxwhiz@gmail.com>
2020-02-08 01:37:15 -0700

    Can now run and record all code.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
 import sys
+import numpy as np
+import matplotlib
 
 import helper_tools
+
+
+def run_file(file_name, out_file):
+	temp = sys.stdout
+	sys.stdout = open(out_file, 'w')
+	try:
+		exec(open(file_name).read())
+		sys.stdout = temp
+		return open(out_file, 'r').read()
+	except Exception:
+		print(f'Could not read {file_name}')
+		sys.stdout = temp
+		return None
 
 
 def py_grader(prob):
@@ -8,9 +24,17 @@ def py_grader(prob):
 	python_files = helper_tools.files.get_files(prob, 'py')
 	run_files = []
 	for file in python_files:
+		temp_out = 'TEMP_OUPUT_JOSH_GRADER.txt'
 		student_name = f'{file.split("_")[1]} {file.split("_")[0]}'
 		source_file = open(file, 'r').read()
-		run_files.append({'name': student_name, 'source': source_file})
+
+		run_files.append({'name': student_name, 'source': source_file, 'out': run_file(file, temp_out)})
+
+	bad_reads = 0
+	for student in run_files:
+		if student['out'] is None:
+			bad_reads += 1
+	print(f'There were {bad_reads} bad reads')
 
 
 def xlsx_grader(prob):
