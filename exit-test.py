84d273d42cfeeae480f388a7cffb86457ff7bbed
josh-laptop <rcxwhiz@gmail.com>
2020-02-14 10:48:09 -0700

    Created a test file with a demo on how the new loop killing will work.

---
+++ exit-test.py
@@ -0,0 +1,19 @@
+# MY CODE PART 1 ################################
+import os
+import time
+import threading
+def kill_prog():
+    time.sleep(1)
+    print('Killed')
+    os._exit(1)
+kill_thread = threading.Thread(target=kill_prog)
+kill_thread.start()
+# ###############################################
+
+print('test')
+while True:
+    i = 0
+
+# MY CODE PART 2 ################################
+os._exit(1)
+# ###############################################
